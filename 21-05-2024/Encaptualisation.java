0afde139dc050ffe46a61d534b416a9e1e706618
RemyaKS <122439886+RemyaKS@users.noreply.github.com>
2024-05-21 17:15:25 +0530

    Add files via upload

---
+++ 21-05-2024/Encaptualisation.java
@@ -0,0 +1,9 @@
+public class Encaptualisation {
+    public static void main(String[] args) {
+      person myObj = new person();
+      myObj.setName("Niha"); 
+      System.out.println(myObj.getName());
+    }
+  }
+    
+
---
+++ 21-05-2024/access.java
@@ -0,0 +1,11 @@
+public class access {
+    final int x = 15;
+    final double PI = 2.80;
+  
+    public static void main(String[] args) {
+        access myObj = new access();
+      System.out.println(myObj.x); 
+      System.out.println(myObj.PI); 
+    }
+  }
+  
