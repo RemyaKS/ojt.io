b6a2a2adc200930e95988097f668cb74679afe9c
RemyaKS <122439886+RemyaKS@users.noreply.github.com>
2024-05-22 17:18:34 +0530

    Add files via upload

--- 21-05-2024/access.java
+++ 21-05-2024/access.java
@@ -1,11 +1,13 @@
 public class access {
-    final int x = 15;
-    final double PI = 2.80;
-  
-    public static void main(String[] args) {
-        access myObj = new access();
-      System.out.println(myObj.x); 
-      System.out.println(myObj.PI); 
-    }
+  final int x = 10;
+  final double PI = 3.14;
+
+  public static void main(String[] args) {
+      access myObj = new access();
+      /*myObj.x = 50; // will generate an error: cannot assign a value to a final variable
+      myObj.PI = 25; // will generate an error: cannot assign a value to a final variable*/
+    System.out.println(myObj.x); 
+    System.out.println(myObj.PI); 
   }
+}
   
