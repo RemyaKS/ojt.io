d1c1b920e359d047433c257a54954ab78d373c11
RemyaKS <122439886+RemyaKS@users.noreply.github.com>
2024-05-24 16:58:11 +0530

    Add files via upload

---
+++ 24-05-2024/multiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class multiplication{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n=s.nextInt();
+        System.out.println("Number ="+n);
+        for(int i=1;i<=12;i++){
+
+            System.out.println(i+"*"+n+"="+(n*i));
+        }
+
+    }
+}
